4473b41c8cec0f040214e63341b18ec15ed698b6
Christoph Hahn <chrhhn@gmail.com>
2022-12-30 02:20:21 +0100

    A Pascal version

---
+++ star.pas
@@ -0,0 +1,19 @@
+program Star;
+
+var
+	x: integer;
+	y: integer;
+
+begin
+  for y := -8 to 9 do
+  begin
+    for x := -8 to 9 do
+    begin
+        if (abs(x) > 4) and (abs(y) > 4) or (abs(y) - abs (x) > 4) or (abs(x) - abs (y) > 4) then
+            write(' ')
+        else
+            write('*');
+    end;
+    writeln('');
+  end;
+end.
